022a64c7ff99221044c78cb575d54b8f16a2e38e
2204852 <154334918+2204852@users.noreply.github.com>
2024-02-04 12:14:27 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -23,7 +23,7 @@ try:
     OWNER = config("OWNER", "943270135")
     FFMPEG = config(
         "FFMPEG",
-        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 30 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
+        default='ffmpeg -i "{}" -preset fast -c:v libx265 -crf 30 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
     )
     TELEGRAPH_API = config("TELEGRAPH_API", default="https://api.telegra.ph")
     THUMB = config(
